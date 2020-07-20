b854f425f923d92da392b9f316ad6491cb1dd817
liudan185 <liudan185@jd.com>
2020-07-20 22:41:44 +0800

    fix: update schemaEntity

--- src/data/schemaEntity/DataSource.js
+++ src/data/schemaEntity/DataSource.js
@@ -25,10 +25,48 @@ export const initDataSourceData = {
     data: {
       type: 'string',
       title: '本地json数据',
+      placeholder: '请输入静态json数据', // 输入提示
       format: 'json',
       default: '{}', // 默认值
       description: '用于设置本地的静态json数据',
       isRequired: true,
+    },
+    filter: {
+      type: 'string',
+      title: '过滤器',
+      format: 'codearea',
+      default: '() => {}',
+      description: '用于定义过滤当前数据的函数',
+      isRequired: true,
+    },
+  },
+  required: ['type', 'data', 'filter'],
+  propertyOrder: ['type', 'data', 'filter'],
+};
+
+// 默认是用于展示local本地数据源的，如果展示远程数据源使用initDataSourceDataV2
+export const initDataSourceDataV2 = {
+  type: 'object',
+  format: 'datasource',
+  title: '数据源',
+  readOnly: false,
+  properties: {
+    type: {
+      type: 'string',
+      default: 'remote',
+      format: 'typeSelect',
+      enum: ['local', 'remote'],
+      enumextra: ['local', 'remote'],
+      title: '数据源类型',
+    },
+    data: {
+      type: 'string',
+      title: '远程json数据',
+      placeholder: '请输入远程json数据源地址', // 输入提示
+      format: 'url',
+      default: 'http://xxx', // 默认值
+      isRequired: true,
+      description: '用于设置获取元素数据的请求地址',
     },
     filter: {
       type: 'string',
