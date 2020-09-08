ab1702b73641f271d0f1c8b8eb61429f1edf991c
liudan185 <liudan185@jd.com>
2020-09-08 18:26:02 +0800

    fix: 修复number默认值为null时的问题

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,7 +13,7 @@ export function isEqual(targetObj, nextTargetObj) {
  * 备注：要识别boolean类型的数值 */
 export function exitPropertie(targetPropertie) {
   let exitPropertie = false;
-  if (targetPropertie !== undefined) {
+  if (targetPropertie !== undefined && targetPropertie !== null) {
     // targetPropertie 等于""、0、false时均认为是存在的属性
     exitPropertie = true;
   }
